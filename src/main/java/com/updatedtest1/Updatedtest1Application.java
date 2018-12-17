9517f254e85eacf836196cc18af5c4565fa2747e
Matt Cadorette <mcadorette@gmail.com>
2018-12-17 14:46:29 -0500

    Initial commit from Atomist

---
+++ src/main/java/com/updatedtest1/Updatedtest1Application.java
@@ -0,0 +1,12 @@
+package com.updatedtest1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Updatedtest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Updatedtest1Application.class, args);
+	}
+}
